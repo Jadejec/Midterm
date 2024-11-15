bb8cff617ced373e78f04e46ec94cea47d4c283c
Jade Cordero <jadejec.jc@gmail.com>
2024-11-16 00:19:56 +0800

    Placing Footer

--- student/register.php
+++ student/register.php
@@ -127,7 +127,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             </table>
         <?php endif; ?>
     </div>
-
+    <?php include "../footer.php"; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
